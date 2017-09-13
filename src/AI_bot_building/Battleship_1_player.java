c18404e099ad5e03b1713237f48b4a2eb4bb1755
Ryunsky <yuanyun.chen@hotmail.com>
2017-09-12 21:36:39 -0400

    add battleship code

--- src/AI_bot_building/Battleship_1_player.java
+++ src/AI_bot_building/Battleship_1_player.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Battleship_1_player {
+
     static BufferedReader reader;
     static BufferedWriter writer;
     static ArrayList<LOC> unknown;
@@ -15,11 +16,12 @@ public class Battleship_1_player {
     static boolean isFirstShoot;
     static int nextShootX;
     static int nextShootY;
+    static int size;
     
     public static void main(String[] args) throws IOException{
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
-        int size = scan.nextInt();
+        size = scan.nextInt();
         String[] boardS = new String[size];
         boardC = new char[size][size];
         for(int i = 0; i<size; i++){
@@ -56,6 +58,7 @@ public class Battleship_1_player {
         
         writer = new BufferedWriter(new FileWriter("state.txt",false));
         writer.write(nextShootX + "," + nextShootY + "," + lastHit.getX() + "," + lastHit.getY());
+        System.err.println(nextShootX + "," + nextShootY + "," + lastHit.getX() + "," + lastHit.getY());
         writer.newLine();
         writer.close();
     }
@@ -98,6 +101,7 @@ public class Battleship_1_player {
             return true;
         }
         
+        
         public int distance (LOC next){
             return Math.abs(this.x-next.x) + Math.abs(this.y-next.y);
         }
@@ -129,60 +133,139 @@ public class Battleship_1_player {
             nextShootX = unknown.get(index).x;
             nextShootY = unknown.get(index).y;
             lastHit = new LOC(10,10);
+            System.err.println("first shoot");
         }
         // haven't shot the target
         else if (boardC[lastTarget.x][lastTarget.y] == 'm'){
+            System.err.println("miss");
             if (!lastHit.isValid()) {
                 nextShootX = unknown.get(index).x;
                 nextShootY = unknown.get(index).y;
             } else {
-//                int minDist = 10;
-//                int tempDist;
-//                int nextShootIndex = 0;
-//                for (int i = 0; i < unknown.size(); i++){
-//                    tempDist = unknown.get(i).distance(new LOC(lastHit.x, lastHit.y));
-//                    if (tempDist < minDist){
-//                        minDist = tempDist;
-//                        nextShootIndex = i;
-//                    }
-//                }
-//                nextShootX = unknown.get(nextShootIndex).x;
-//                nextShootY = unknown.get(nextShootIndex).y;
-                int minDist = 10;
-                int tempDist;
-                int nextShootIndex = 0;
-                if (lastTarget.x == lastHit.x){
-                    for (int i = 0; i < unknown.size(); i++){
-                        tempDist = unknown.get(i).horizontalDist(new LOC(lastTarget.x, lastTarget.y));
-                        if (tempDist < minDist){
-                            minDist = tempDist;
-                            nextShootIndex = i;
-                        }
-                    }
-                }
-                else {
-                    for (int i = 0; i < unknown.size(); i++){
-                        tempDist = unknown.get(i).verticalDist(new LOC(lastTarget.x, lastTarget.y));
-                        if (tempDist < minDist){
-                            minDist = tempDist;
-                            nextShootIndex = i;
-                        }
-                    }
-                }
-              nextShootX = unknown.get(nextShootIndex).x;
-              nextShootY = unknown.get(nextShootIndex).y;
+              //same row
+              if (lastTarget.x == lastHit.x){
+                  boolean isFound = false;
+                  //miss is on the left of the last hit
+                  if(lastTarget.y < lastHit.y) {
+                      for (int i = lastHit.y+1; i < size-1; i++){
+                          if(boardC[lastHit.x][i] =='h'){
+                              continue;
+                          }
+                          if (boardC[lastHit.x][i]!='-') {
+                              break;
+                          }
+                          if(boardC[lastHit.x][i] =='-') {
+                              nextShootX = lastHit.x;
+                              nextShootY = i;
+                              isFound = true;
+                              break;
+                          }
+                      }
+                  }
+                  //miss is on the right of the last hit
+                  else if (lastTarget.y > lastHit.y) {
+                      for (int i = lastHit.y-1; i > 0; i--){
+                          if(boardC[lastHit.x][i] =='h'){
+                              continue;
+                          }
+                          if (boardC[lastHit.x][i]!='-') {
+                              break;
+                          }
+                          if(boardC[lastHit.x][i] =='-') {
+                              nextShootX = lastHit.x;
+                              nextShootY = i;
+                              isFound = true;
+                              break;
+                          }
+                      }
+                  }
+                  if (!isFound){
+                      if (lastHit.x == 0){
+                          nextShootX = lastHit.x +1;
+                          nextShootY = lastHit.y;
+                      } else if (lastHit.x ==9){
+                          nextShootX = lastHit.x - 1;
+                          nextShootY = lastHit.y;
+                      } else {
+                          if (boardC[lastHit.x-1][lastHit.y] == '-'){
+                              nextShootX = lastHit.x - 1;
+                              nextShootY = lastHit.y;
+                          } else {
+                              nextShootX = lastHit.x +1;
+                              nextShootY = lastHit.y;
+                          }
+                      }
+                  }
+              }
+              //same column
+              else if (lastTarget.y == lastHit.y){
+                  boolean isFound = false;
+                  //miss is on the left of the last hit
+                  if(lastTarget.x < lastHit.x) {
+                      for (int i = lastHit.x+1; i < size-1; i++){
+                          if(boardC[i][lastHit.y] =='h'){
+                              continue;
+                          }
+                          if(boardC[i][lastHit.y] !='-') {
+                              break;
+                          }
+                          if(boardC[i][lastHit.y] =='-') {
+                              nextShootX = i;
+                              nextShootY = lastHit.y;
+                              isFound = true;
+                              break;
+                          }
+                      }
+                  }
+                  //miss is on the right of the last hit
+                  else if (lastTarget.x > lastHit.x) {
+                      for (int i = lastHit.x-1; i > 0; i--){
+                          if(boardC[i][lastHit.y] =='h'){
+                              continue;
+                          }
+                          if(boardC[i][lastHit.y] !='-') {
+                              break;
+                          }
+                          if(boardC[i][lastHit.y] =='-') {
+                              nextShootX = i;
+                              nextShootY = lastHit.y;
+                              isFound = true;
+                              break;
+                          }
+                      }
+                  }
+                  if (!isFound){
+                      if (lastHit.y == 0){
+                          nextShootX = lastHit.x;
+                          nextShootY = lastHit.y +1;
+                      } else if (lastHit.y == 9){
+                          nextShootX = lastHit.x;
+                          nextShootY = lastHit.y - 1;
+                      } else {
+                          if (boardC[lastHit.x][lastHit.y - 1] == '-'){
+                              nextShootX = lastHit.x;
+                              nextShootY = lastHit.y - 1;
+                          } else {
+                              nextShootX = lastHit.x;
+                              nextShootY = lastHit.y +1;
+                          }
+                      }
+                  }
+              }
+
             }
         } 
         // Destroy the target
         else if (boardC[lastTarget.x][lastTarget.y] == 'd') {
+            System.err.println("destory");
             boolean isHit = false;
             lastHit.setX(10);
             lastHit.setY(10);;
             //check if there is any hitted location. 
             for (int i = 0; i < known.size();i++){
                 if (boardC[known.get(i).getX()][known.get(i).getY()] == 'h'){
-                    lastHit.setX(known.get(i).getX());
-                    lastHit.setY(known.get(i).getY());
+                    lastTarget.setX(known.get(i).getX());
+                    lastTarget.setY(known.get(i).getY());
                     isHit = true;
                     break;
                 }
@@ -197,9 +280,11 @@ public class Battleship_1_player {
         
         else if (boardC[lastTarget.x][lastTarget.y] == 'h'){
             if (lastHit.isValid()) {
+                System.err.println("already hit");
                 alreadyHit();
             } else {
                 firstHit();
+                System.err.println("first hit");
             }
         }
     }
@@ -224,30 +309,141 @@ public class Battleship_1_player {
     }
     
     public static void alreadyHit(){
-        int minDist = 10;
-        int tempDist;
-        int nextShootIndex = 0;
+        //same row
         if (lastTarget.x == lastHit.x){
-            for (int i = 0; i < unknown.size(); i++){
-                tempDist = unknown.get(i).horizontalDist(new LOC(lastTarget.x, lastTarget.y));
-                if (tempDist < minDist){
-                    minDist = tempDist;
-                    nextShootIndex = i;
+            boolean isFound = false;
+            //last target is on the left of the last hit
+            if(lastTarget.y < lastHit.y) {
+                if (lastTarget.y != 0 && boardC[lastTarget.x][lastTarget.y-1] == '-'){
+                    nextShootX = lastHit.x;
+                    nextShootY = lastTarget.y -1;
+                    isFound = true;
+                } else {
+                    for (int i = lastHit.y+1; i < size-1; i++){
+                        if(boardC[lastTarget.x][i] =='h'){
+                            continue;
+                        }
+                        if(boardC[lastHit.x][i] !='-') {
+                            break;
+                        }
+                        if(boardC[lastHit.x][i] =='-') {
+                            nextShootX = lastHit.x;
+                            nextShootY = i;
+                            isFound = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            //miss is on the right of the last hit
+            else if (lastTarget.y > lastHit.y) {
+                if (lastTarget.y != 9 && boardC[lastTarget.x][lastTarget.y+1] == '-'){
+                    nextShootX = lastHit.x;
+                    nextShootY = lastTarget.y + 1;
+                    isFound = true;
+                } else {
+                    for (int i = lastHit.y-1; i > 0; i--){
+                        if(boardC[lastHit.x][i] =='h'){
+                            continue;
+                        }
+                        if(boardC[lastHit.x][i] !='-'){
+                            break;
+                        }
+                        if(boardC[lastHit.x][i] =='-') {
+                            nextShootX = lastHit.x;
+                            nextShootY = i;
+                            isFound = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!isFound){
+                if (lastHit.x == 0){
+                    nextShootX = lastHit.x +1;
+                    nextShootY = lastHit.y;
+                } else if (lastHit.x == 9){
+                    nextShootX = lastHit.x - 1;
+                    nextShootY = lastHit.y;
+                } else {
+                    if (boardC[lastHit.x-1][lastHit.y] == '-'){
+                        nextShootX = lastHit.x - 1;
+                        nextShootY = lastHit.y;
+                    } else {
+                        nextShootX = lastHit.x +1;
+                        nextShootY = lastHit.y;
+                    }
                 }
             }
         }
-        else {
-            for (int i = 0; i < unknown.size(); i++){
-                tempDist = unknown.get(i).verticalDist(new LOC(lastTarget.x, lastTarget.y));
-                if (tempDist < minDist){
-                    minDist = tempDist;
-                    nextShootIndex = i;
+        //same column
+        else if (lastTarget.y == lastHit.y){
+            boolean isFound = false;
+            //miss is on the left of the last hit
+            if(lastTarget.x < lastHit.x) {
+                if (lastTarget.x != 0 && boardC[lastTarget.x -1][lastTarget.y] == '-'){
+                    System.err.println("more option");
+                    nextShootX = lastTarget.x -1;
+                    nextShootY = lastTarget.y;
+                    isFound = true;
+                } else {
+                    for (int i = lastHit.x+1; i < size-1; i++){
+                        if(boardC[i][lastHit.y] =='h'){
+                            continue;
+                        }
+                        if(boardC[i][lastHit.y] !='-') {
+                            break;
+                        }
+                        if(boardC[i][lastHit.y] =='-') {
+                            nextShootX = i;
+                            nextShootY = lastHit.y;
+                            isFound = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            //miss is on the right of the last hit
+            else if (lastTarget.x > lastHit.x) {
+                if (lastTarget.x != 9 && boardC[lastTarget.x + 1][lastTarget.y] == '-'){
+                    nextShootX = lastTarget.x + 1;
+                    nextShootY = lastTarget.y;
+                    isFound = true;
+                } else {
+                    for (int i = lastHit.x-1; i > 0; i--){
+                        if(boardC[i][lastHit.y] =='h'){
+                            continue;
+                        }
+                        if(boardC[i][lastHit.y] =='-') {
+                            nextShootX = i;
+                            nextShootY = lastHit.y;
+                            isFound = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!isFound){
+                if (lastHit.y == 0){
+                    nextShootX = lastHit.x;
+                    nextShootY = lastHit.y +1;
+                } else if (lastHit.y == 9){
+                    nextShootX = lastHit.x;
+                    nextShootY = lastHit.y - 1;
+                } else {
+                    if (boardC[lastHit.x][lastHit.y - 1] == '-'){
+                        nextShootX = lastHit.x;
+                        nextShootY = lastHit.y - 1;
+                    } else {
+                        nextShootX = lastHit.x;
+                        nextShootY = lastHit.y +1;
+                    }
                 }
             }
         }
         lastHit.setX(lastTarget.x);
         lastHit.setY(lastTarget.y);
-        nextShootX = unknown.get(nextShootIndex).x;
-        nextShootY = unknown.get(nextShootIndex).y;
+      }
     }
-}
+
+

46a2243fd53b00360b354181061858c6f0ea6248
Ryunsky <yuanyun.chen@hotmail.com>
2017-09-14 15:35:48 -0400

    coorected the class name for ChessboardGame
    Change-Id: Icc95db3fa13248239fde114bf97b935378974c3c

--- src/GameTheory/ChessboardGame.java
+++ src/GameTheory/ChessboardGame.java
@@ -1,8 +1,8 @@
-
+package GameTheory;
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class ChessboardGame {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */

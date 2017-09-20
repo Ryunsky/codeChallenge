17622c288a0f077ef4ccae36b8fa910fc0a68445
Ryunsky <yuanyun.chen@hotmail.com>
2017-09-19 22:17:06 -0400

    add tower breaker code

--- src/GameTheory/TowerBreakerAgain.java
+++ src/GameTheory/TowerBreakerAgain.java
@@ -3,19 +3,21 @@ package GameTheory;
 import java.io.*;
 import java.util.*;
 
-public class TowerBeaker {
+public class TowerBreakerAgain {
+
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. 
          * Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
         int games = scan.nextInt();
-        while(games-->=0){
+        while(games-->0){
             int tower = scan.nextInt();
             int result = 0;
             for (int i = 0; i < tower; i++){
                 int level = scan.nextInt();
-                result ^= totalPrimes(level);
+                boolean isTwo = true;
+                result ^= totalOddPrimes(level, isTwo);
             }
             if(result ==0){
                 System.out.println(2);
@@ -26,16 +28,20 @@ public class TowerBeaker {
         scan.close();
     }
     
-    private static int totalPrimes (int i){
+    private static int totalOddPrimes (int i, boolean isTwo){
         if(i == 0 || i == 1){
             return 0;
         }
         if(i%2 == 0){
-            return (1 + totalPrimes(i/2));
+            if(isTwo == true){
+                return (1+ totalOddPrimes(i/2, false));
+            } else{
+                return totalOddPrimes(i/2, false);
+            }
         } else {
             for(int j = 3; j*j <= i; j+=2){
                 if(i%j==0){
-                    return (1 + totalPrimes(i/j));
+                    return (1 + totalOddPrimes(i/j,false));
                 }
             }
         }

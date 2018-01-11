d10f759b8ebb9b28df9f93051e96fdf2c8745b15
Ryunsky <yuanyun.chen@hotmail.com>
2018-01-11 14:57:59 -0500

    add two sum problem into LeeCode
    Change-Id: I9c2e63dc8f24d9af1725909ab263c50bd37f7ae3

---
+++ src/LeeCode/TwoSum.java
@@ -0,0 +1,30 @@
+package LeeCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] tmp = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                tmp[1] = i;
+                tmp[0] = map.get(target - nums[i]);
+            }
+            map.put(nums[i], i);
+        }
+        return tmp;
+    }
+}
